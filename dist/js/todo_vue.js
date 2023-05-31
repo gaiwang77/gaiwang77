62d8aa66967e745ad4952563ed9de2bc1e56d91c
gaiwang77 <gaiwang48@gmail.com>
2023-05-31 21:35:13 +0800

    33

--- dist/js/todo_vue.js
+++ dist/js/todo_vue.js
@@ -1,10 +1,27 @@
-Vue.createApp({
+let vm = Vue.createApp({
     data() {
         return {
-            name: '888'
+            pending: [],
+            done: [],
+            itemValue: '',
         }
     },
     methods: {
-
+        doAddItem() {
+            let value = this.itemValue;
+            if (!value) {
+                Swal.fire({
+                    title: '新增錯誤',
+                    text: '請輸入項目名稱',
+                    icon: 'error',
+                }).then(() => {
+                    setTimeout(() => {
+                        this.$refs.itemValue.focus();
+                    }, 500)
+                })
+                return;
+            }
+            this.pending.push(value);
+        }
     }
 }).mount('#app');
