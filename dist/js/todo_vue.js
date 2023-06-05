772ba033ac1e030bd77846e665f1c7a4cbbd078b
gaiwang77 <gaiwang48@gmail.com>
2023-06-05 20:28:31 +0800

    F12

--- dist/js/todo_vue.js
+++ dist/js/todo_vue.js
@@ -1,4 +1,17 @@
+const database = {
+    set(key, value) {
+        localStorage.setItem(key, value);
+    },
+    get(key) {
+        return localStorage.getItem(key);
+    },
+    remove(key) {
+        localStorage.removeItem(key);
+    }
+}
+
 let vm = Vue.createApp({
+    
     data() {
         return {
             pending: [],
