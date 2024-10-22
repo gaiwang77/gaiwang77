a6bbde9151d0c32800a844e03c4885cb2e682fc3
gaiwang77 <gaiwang48@gmail.com>
2024-10-22 13:47:27 +0800

    ann.computer

---
+++ src/List.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps(["products"]);
+</script>
+
+<template>
+<div class="list" >
+    <div class="product" v-for="product in products" >
+        <div class="name">產品名稱:{{ product.name }}</div>
+        <div class="des">產品描述:{{ product.description }}</div>
+        <div class="price">價格:${{ product.price }}</div>
+    </div>
+</div>
+</template>
+
+ <style scoped>
+
+</style>
---
+++ src/Page.vue
@@ -0,0 +1,25 @@
+<script setup>
+// https://cwpeng.github.io/live-records-samples/data/products.json
+//https://cwpeng.github.io/live-records-samples/data/reviews.json
+import {onMounted, ref } from 'vue';
+import List from './List.vue';
+let products=ref(null);//建立空值，來抓JSON
+//組件掛載完成後，開始呼叫fetch串接後端的資料
+onMounted(async function(){
+let response=await fetch("https://cwpeng.github.io/live-records-samples/data/products.json");
+let data=await response.json();
+products.value=data;
+});
+</script>
+
+<template>
+<div class="headline">產品資料列表</div>
+<div  v-if="products===null">資料載入中</div>
+<List v-else :products="products">
+    
+</List>
+</template>
+
+<style scoped>
+
+</style>
